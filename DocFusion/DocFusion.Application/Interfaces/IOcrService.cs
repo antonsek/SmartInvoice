1e8e07aebee3fc93d89361332958545db1ab3f70
anton sek <antonsek96@gmail.com>
2025-10-30 21:00:12 +0500

    ocr service

---
+++ DocFusion/DocFusion.Application/Interfaces/IOcrService.cs
@@ -0,0 +1,6 @@
+﻿namespace DocFusion.Application.Interfaces;
+
+public interface IOcrService
+{
+    string ExtractText(Stream fileStream, string fileName);
+}
--- DocFusion/DocFusion.Infrastructure/Services/TesseractOcrService.cs
+++ DocFusion/DocFusion.Infrastructure/Services/TesseractOcrService.cs
@@ -1,6 +1,61 @@
-﻿namespace DocFusion.Infrastructure.Services;
+﻿using DocFusion.Application.Interfaces;
+using Tesseract;
 
-public class TesseractOcrService
+namespace DocFusion.Infrastructure.Services;
+
+public class TesseractOcrService : IOcrService
 {
+    private readonly string _tessDataPath;
+
+    public TesseractOcrService(string tessDataPath)
+    {
+        _tessDataPath = tessDataPath;
+    }
+
+    public string ExtractText(Stream fileStream, string fileName)
+    {
+        string extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+        return extension switch
+        {
+            ".pdf" => ExtractFromPdf(fileStream),
+            _ => ExtractFromImage(fileStream)
+        };
+    }
+
+    private string ExtractFromImage(Stream stream)
+    {
+        using var engine = new TesseractEngine(_tessDataPath, "eng+rus", EngineMode.Default);
+        using var tempFile = new TempFile(stream);
+        using var img = Pix.LoadFromFile(tempFile.Path);
+
+        using var page = engine.Process(img);
+        Console.WriteLine($"🖼️ OCR Confidence: {page.GetMeanConfidence() * 100:0.0}%");
+        return page.GetText();
+    }
+
+    private string ExtractFromPdf(Stream pdfStream)
+    {
+        return "";
+    }
     
+    // --- вспомогательные классы ---
+    private sealed class TempFile : IDisposable
+    {
+        public string Path { get; }
+
+        public TempFile(Stream input)
+        {
+            Path = System.IO.Path.GetTempFileName();
+            using var fs = File.Create(Path);
+            input.CopyTo(fs);
+            fs.Flush();
+        }
+
+        public void Dispose()
+        {
+            try { File.Delete(Path); } catch { }
+        }
+    }
 }
+
