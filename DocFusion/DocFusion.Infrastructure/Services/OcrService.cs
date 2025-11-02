fe036b08bde2b1f957155183c6090428554ad440
antonsek <44634016+antonsek@users.noreply.github.com>
2025-11-02 22:57:57 +0500

    add license and language

--- DocFusion/DocFusion.Infrastructure/Services/OcrService.cs
+++ DocFusion/DocFusion.Infrastructure/Services/OcrService.cs
@@ -9,7 +9,12 @@ public class OcrService : IOcrService
 
     public OcrService()
     {
+
+        IronOcr.License.LicenseKey = "test";
         _ocr = new IronTesseract();
+        _ocr.Language = OcrLanguage.Russian;
+        _ocr.AddSecondaryLanguage(OcrLanguage.English);
+        _ocr.AddSecondaryLanguage(OcrLanguage.Kazakh);
     }
 
     public string ExtractText(Stream fileStream, string fileName)
@@ -28,7 +33,7 @@ public class OcrService : IOcrService
         using var input = new OcrInput();
         input.LoadImage(stream);
 
-        var result = _ocr.Read(input);
+        var result = _ocr.ReadDocument(input);
         Console.WriteLine($"🖼️ OCR Confidence: {result.Confidence:0.0}%");
         return result.Text;
     }
@@ -37,8 +42,8 @@ public class OcrService : IOcrService
     {
         using var input = new OcrInput();
         input.LoadPdf(pdfStream);
-
-        var result = _ocr.Read(input);
+        
+        var result = _ocr.ReadDocument(input);
         Console.WriteLine($"📄 PDF OCR Confidence: {result.Confidence:0.0}%");
         return result.Text;
     }
